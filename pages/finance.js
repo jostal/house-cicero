4f8ecd9a65e8ffb0988dcef3871961673654b482
Joseph Talon <josephtalon01@gmail.com>
2022-02-10 13:19:54 -0500

    Added link to api

--- pages/finance.js
+++ pages/finance.js
@@ -4,6 +4,7 @@ export default function finance() {
   
   return (
     <div>
+      <p>Debts not loading? Click <a href="https://99.225.180.173:1337">here</a></p>
       <OwedAmounts />
     </div>
   )
